884d925727d82379cf35811372a55ff1fd0c55bd
Raveen SIngh <Raveen@Raveens-MacBook-Pro.local>
2018-08-10 10:39:57 -0400

    setup db & mongoose

---
+++ models/transacation.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const transcationScema = new Schema({
+  date: String, id: String, amountF: String, amountBch: String, status: String,
+});
+mongoose.model('transaction', transcationScema);
