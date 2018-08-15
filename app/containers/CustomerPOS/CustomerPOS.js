391b72238c8ec2bb2c309750efb8acf09fb2484f
raveen-singh <emailraveensingh@gmail.com>
2018-08-14 23:05:09 -0400

    starting to implement socket

--- app/containers/CustomerPOS/CustomerPOS.js
+++ app/containers/CustomerPOS/CustomerPOS.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import './style.scss';
+import IMG from '../../images/bitcoin-bay.jpg';
 
 export default class CustomerPOS extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
-      <h1>Hello</h1>
+      <div className="payment">
+        <img src={IMG} height="400" width="400" />
+      </div>
     );
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,16 +1,15 @@
 /* eslint consistent-return:0 */
 
-const express = require('express');
+const app = require('express')();
 const mongoose = require('mongoose');
-const logger = require('./util//logger');
-const argv = require('./util/argv');
 const port = require('./util//port');
 const setup = require('./middlewares/frontendMiddleware');
-const { resolve } = require('path');
+const {resolve} = require('path');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-
-const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const argv = require('./util/argv');
 
 // If you need a backend, e.g. an API, add your custom backend-specific
 // middleware here app.use('/api', myApi); In production we need to pass these
@@ -25,27 +24,27 @@ setup(app, {
 const customHost = argv.host || process.env.HOST;
 const host = customHost || null; // Let http.Server use its default IPv6/4 host
 const prettyHost = customHost || 'localhost';
+const logger = require('./util//logger');
 
 passport.use(new LocalStrategy(((username, password, done) => {
-  User
-    .findOne({
-      username,
-    }, (err, user) => {
-      if (err) {
-        return done(err);
-      }
-      if (!user) {
-        return done(null, false, {message: 'Incorrect username.'});
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, {message: 'Incorrect password.'});
-      }
-      return done(null, user);
-    });
+  User.findOne({
+    username,
+  }, (err, user) => {
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      return done(null, false, { message: 'Incorrect username.' });
+    }
+    if (!user.validPassword(password)) {
+      return done(null, false, { message: 'Incorrect password.' });
+    }
+    return done(null, user);
+  });
 })));
 
 // Start your app.
-app.listen(port, host, (err) => {
+http.listen(port, host, (err) => {
   if (err) {
     return logger.error(err.message);
   }
@@ -56,3 +55,7 @@ app.post('/login', passport.authenticate('local', {
   failureRedirect: '/login',
   failureFlash: true,
 }));
+
+io.on('connection', (socket) => {
+  console.log('hello');
+});
